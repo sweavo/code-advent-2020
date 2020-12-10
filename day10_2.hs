d25d37cf4e5bc16768d54aa2dd43f33bc80c76d6
Steve Carter <steve.carter@etas.com>
2020-12-10 20:30:29 +0000

    it is going too long between commits during this refactor

--- day10_2.hs
+++ day10_2.hs
@@ -1,56 +1,61 @@
 import Data.List as List
 
--- design notes: I'm taking a pass through the _prepare_d data, figuring out 
--- how many of the preceding values I care about and summing the paths to those
--- In the case where the sequence is just [ 0 ], there is only 1 way to get to
--- there, 
 example1=[ 16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4 ]
 
 example2=[ 28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3 ]
 
-day10input = [115,134,121,184,78,84,77,159,133,90,71,185,152,165,39,64,85,
-             50,20,75,2,120,137,164,101,56,153,63,70,10,72,37,
-             86,27,166,186,154,131,1,122,95,14,119,3,99,172,111,
-             142,26,82,8,31,53,28,139,110,138,175,108,145,58,76,
-             7,23,83,49,132,57,40,48,102,11,105,146,149,66,38,155,
-             109,128,181,43,44,94,4,169,89,96,60,69,9,163,116,45,
-             59,15,178,34,114,17,16,79,91,100,162,125,156,65]
+day10input = [ 38, 23, 31, 16, 141, 2, 124, 25, 37, 147, 86, 150, 99, 75, 81, 
+    121, 93, 120, 96, 55, 48, 58, 108, 22, 132, 62, 107, 54, 69, 51, 7, 134, 
+    143, 122, 28, 60, 123, 82, 95, 14, 6, 106, 41, 131, 109, 90, 112, 1, 103, 
+    44, 127, 9, 83, 59, 117, 8, 140, 151, 89, 35, 148, 76, 100, 114, 130, 19, 
+    72, 36, 133, 12, 34, 46, 15, 45, 87, 144, 80, 13, 142, 149, 88, 94, 61, 
+    154, 24, 66, 113, 5, 73, 79, 74, 65, 137, 47 ]
 
+-- thanks, Jack.
 prepare input = List.sort ([0, 3 + maximum input] ++ input)
 
-can_reach a b = ((a + 1) <= b) && (b <= (a + 3))
+-- Design notes: we want to make a single pass through the input. Because of
+-- lazy evaluation we can regard list expressions as iterators.  First we find
+-- for each element of the input the number of preceding elements that were 
+-- within the constraint of 3 jolts.  Then we run a stack machine over those
+-- numbers that sums x[i] numbers from the stack and pushes the result.
 
--- rangechecker accepts a range and returns a function that checks ordered lists
--- are within it
-rangechecker range xs = last xs - head xs <= range
+-- range_check accepts a range and returns a function that tests whether an 
+-- ordered list is within it.
+range_check range xs = last xs - head xs <= range
+day10_2_rule = range_check 3
 
 -- return a list that matches condition, removing elements from the head as 
--- necessary
-beheadUntil condition (h:tail) | condition (h:tail) = (h:tail)
-                               | otherwise          = beheadUntil condition tail
+-- necessary.
+behead_until cond items | cond items = items
+                        | otherwise  = behead_until cond (drop 1 items)
 
--- For each element in list, return the list ending in that element that 
+-- For each element in list, return the list ending at that element that 
 -- satisfies condition.
-conformingSubsequences condition [] _ = []
-conformingSubsequences condition (h:tail) previous = let candidate = beheadUntil condition (previous ++ [h]) in candidate:conformingSubsequences condition tail candidate
-
--- design notes: we could pass through the subsequences, adding the last n items of our accumulator and appending to the accumulator where n is the length of the found subsequence. It seems wasteful but I think that's the right approach. Because working at the head is cheaper, I'll accumulate the totals in reverse order.
-
-day10_2_rule = rangechecker 3
-
-subsequenceLengths xs = map length (conformingSubsequences day10_2_rule (prepare xs) [])
+conformingSubsequences cond [] _ = [] -- to prevent the compiler complaining. It's likely a problem if you hit this case.
+conformingSubsequences cond (h:tail) previous = 
+    let candidate = behead_until cond (previous ++ [h])
+    in candidate:conformingSubsequences cond tail candidate
 
 
-routeCount [] (h:tail) = h
-routeCount (h:tail) accumulator = routeCount tail ((sum (take h accumulator)):accumulator)
+subsequenceLengths xs = map (\x -> length x - 1) (conformingSubsequences day10_2_rule (prepare xs) [])
 
+routeCount stack_func (0:tail) _       = routeCount stack_func tail [1] -- first item has one route to it
+routeCount stack_func (h:tail) stack   = routeCount stack_func tail (stack_func h stack)
+routeCount stack_func [] stack         = head stack -- nothing left to consume, answer is on the top of the stack
+
+day10_2_stack_transform h stack = ((sum (take h stack)):stack)
+
+day10_2_machine input = routeCount day10_2_stack_transform (subsequenceLengths input) []
 
 tests = [
+    behead_until (range_check 4) [0, 2, 3, 5, 7] == [3, 5, 7],
+    behead_until (range_check 5) [0, 2, 3, 5, 7] == [2, 3, 5, 7],
     take 3 ['a','b','c','d'] == ['a','b','c'],
-    conformingSubsequences (rangechecker 3) [0, 2, 3, 5, 8, 9] [] == [[0], [0,2],[0,2,3],[2,3,5],[5,8],[8,9]],
-    routeCount (subsequenceLengths example1) [] == 8,
-    routeCount (subsequenceLengths example2) [] == 19208
+    conformingSubsequences (range_check 3) [0, 2, 3, 5, 8, 9] [] == [[0], [0,2],[0,2,3],[2,3,5],[5,8],[8,9]],
+    conformingSubsequences (\x -> 3 > length x) [0, 2, 3, 5, 8, 9] [] == [[0], [0,2],[2,3],[3,5],[5,8],[8,9]],
+    day10_2_machine example1 == 8,
+    day10_2_machine example2 == 19208,
+    day10_2_machine day10input == 3543369523456
     ]
-
-day10_2 = routeCount (subsequenceLengths day10input) []
 
