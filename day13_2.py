935e962476fbba3701ebbb10f7a50afd2d8a5a5d
Steve Carter <steve.carter@etas.com>
2020-12-13 14:29:05 +0000

    remove rantings of madman and replace with a semblence of competent reasoning.

--- day13_2.py
+++ day13_2.py
@@ -4,72 +4,56 @@ from math import gcd
 
 import day13input
 
-""" For part 2 it feels like some math can avoid a lot of hacking.  This smells
-like Seive of Eratosthenes. For example, solutions are not going to exist any
-place except where x % buses[0] is zero. So a loop can step by buses[0].
+""" This is essentially a problem of combining period and phase of signals.
 
-In fact, solutions can only be at places where the longest period appears at 
-its correct offset.  So a first pass can find offset, maxperiod and then look
-for solutions at (maxperiod * i in POSITIVE_INTS) - offset
+First, observe that the bus numbers are periods T_i, and that their positions
+in the input are their offset c_i. (As presented, c_i == i but I will be 
+reordering the buses below.) Solutions are found where 
+
+    y = ( x + c ) % T
+
+crosses zero. So solutions can only exist at:
+
+    x = ( i * T ) - c for integer i
+
+If we start with the largest period, we are leaping towards the solution in the
+shortest number of steps. So I tag the buses with their indexes (now, offsets)
+and sort them by period. When I consider the first two signals together, I know
+that the solution if any occurs at a period T_r = lcm(T_0, T_1) and that gives
+me an upper bound for finding the point at which both f(T_0,c_0) and f(T_0,c_0)
+are zero. The lower bound is the first solution of the larger curve, i.e. 
+T_0 - c_0.
+
+I tried to do some more mathing to calculate the number of cycles of T_0 needed
+to reach that solution based on the phase and period relationships of the two
+signals, but I was defeated and had to go lie down just to recover the ability
+to count or spell. So, I just brute-force by checking all values such that
+
+    T_0-c_0 <= x < T_r-c_0  &&  x in ( T_0 * PositiveIntegers )
+
+This solution is itself a signal, with period T_r and offset c_r where
+
+    c_r = T_r - x found as above.
+
+This means we just need a function to combine a longer with a shorter signal,
+and we can apply it to the whole sequence of buses with functools.reduce()
 """
-x=None # "x" appears in the input data. This makes it valid syntactically
 
-EXAMPLE_INPUT=(939,
-    [7, 13, x, x, 59, x, 31, 19])
+EXAMPLE_BUSES=[7, 13, None, None, 59, None, 31, 19]
 
 def prep_input(buses):
-    """ Sort the buses by their reverse period, and add their position in the
-    input, which is also the offset from the solution at which they will appear
-    >>> list(prep_input(EXAMPLE_INPUT[1]))
+    """ Sort the buses reversed by their period, having tagged them with their
+    position in the sequence, which is their c value.
+    >>> list(prep_input(EXAMPLE_BUSES))
     [(59, 4), (31, 6), (19, 7), (13, 1), (7, 0)]
     """
-    return sorted([(bus, offset) for offset, bus in enumerate(buses) if bus], reverse=True)
+    return sorted([(bus, offset) 
+                    for offset, bus 
+                    in enumerate(buses) 
+                    if bus], reverse=True)
 
-""" In the first example above, our first candidate is at 55, since it is 4 minutes before the 59 arrives.  Is it also 6 minutes before 31 arrives? No, it's 7 minutes before that, so ... next candidate is 55+59=114...
-"""
-
-def check_answer(buses, timestamp):
-    """ Do all the listed buses leave at the offsets given from timestamp?
-    Return the index of the first wrong answer, or len(buses) for OK
-    >>> check_answer(prep_input([17,x,13,19]), 3400)
-    0
-    >>> check_answer(prep_input([17,x,13,19]), 3417)
-    3
-    >>> check_answer(prep_input([67,7,59,61]), 754018)
-    4
-    >>> check_answer(prep_input([67,x,7,59,61]), 779210)
-    4
-    >>> check_answer(prep_input([67,7,x,59,61]), 1261476)
-    4
-    >>> check_answer(prep_input([1789,37,47,1889]), 1202161486)
-    4
-    """
-    for i, bus in enumerate(buses):
-        mod, offs = bus
-        if ( timestamp + offs ) % mod:
-            return i
-    return i + 1
-
-""" Thinking about interference patterns, two periods shift phase over a certain
-period too. The period of that phase shift relates to the longer period and the reciprocal of the difference in periods.
-
-In fact, each incorrect result can know how many leaps it will take to match
-the longest period. So we can treat it like a combination lock with a tension
-wrench: find the period of the first item, check the second, derive a new 
-period to search so that the foregoing items are correct.
-
-        ** coffee, bagel, and an hour playing with the cat later **
-
-There are two ways to look at it, a set intersection of the zero crossing
-points of each signal, or period and phase of each signal.  Taking the period
-and phase view, we start with the longest period and find the first point 
-indicated by its phase (=period-offset), then check the signal of the next 
-highest period. If that's not at the correct offset, we keep adding period 
-until it locks in.  Then our new period is lcm( period, period_new ) and we 
-proceed with checking the next item.
-"""
 def lcm(x, y):
-    """
+    """ period of combined signal is lcm of the periods of its components
     >>> lcm(3, 9)
     9
     >>> lcm(4, 9)
@@ -78,9 +62,7 @@ def lcm(x, y):
     return x*y//gcd(x,y)
 
 def combine_signals(longer,shorter):
-    """ Each signal is a period, phase tuple. Return another period, phase
-    tuple for the combination. tuple is (t,c) and signal is y = (x+c)%t. For
-    all signals, the correct answer x is where y is zero.
+    """ 
     >>> combine_signals((5,2), (3,1))
     (15, 7)
     >>> combine_signals((3,1), (2,0))
@@ -88,25 +70,29 @@ def combine_signals(longer,shorter):
     >>> combine_signals((13,1),(12,0))
     (156, 144)
     """
-    t0, c0 = longer
-    t1, c1 = shorter
+    T_0, c_0 = longer
+    T_1, c_1 = shorter
 
     # Period is the lcm of the provided periods
-    tr = lcm(t0,t1)
-    # Search for phase within the first new period from the start position
-    for i in range(t0-c0,tr+c0,t0):
-        v0 = (i + c0) % t0
-        v1 = (i + c1) % t1
+    T_result = lcm(T_0,T_1)
+    
+    # Determine phase by searching soutions of longer that fall between the
+    # start position and start + T_result
+    for i in range(T_0-c_0,T_result+c_0,T_0):
+        v0 = (i + c_0) % T_0
+        v1 = (i + c_1) % T_1
         if not( v0 or v1):
-            return (tr,tr-i)
+            return (T_result,T_result-i)
 
 def solve_buses(prepared_buses):
-    """
-    >>> solve_buses(prep_input(EXAMPLE_INPUT[1]))
+    """ Reduce a bunch of periodic signals to a single signal. The value of x
+    that answers the puzzle is the first place ( c + x ) % T = 0, that is to
+    say, c + x = T, or x = T-c.
+    >>> solve_buses(prep_input(EXAMPLE_BUSES))
     1068781
     """
-    t,c = functools.reduce(combine_signals, prepared_buses)
-    return t-c
+    T, c = functools.reduce(combine_signals, prepared_buses)
+    return T - c
 
 def day13_2():
     """
@@ -114,5 +100,4 @@ def day13_2():
     539746751134958
     """
     return solve_buses(prep_input(day13input.BUSES[1]))
-
 
