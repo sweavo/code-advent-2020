a7291d45eeff503193b1cca9ab3b92cee035b744
Steve Carter <steve.carter@etas.com>
2020-12-14 06:52:02 +0000

    looking solvedy

---
+++ day14_1.py
@@ -0,0 +1,62 @@
+EXAMPLE_DOCKING_PROGRAM = [
+    "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+    "mem[8] = 11",
+    "mem[7] = 101",
+    "mem[8] = 0" ]
+
+def mask_decode(tristatestring):
+    """ Translate a tristate mask into two masks: one to be _or_ed and one to 
+    be _and_ed
+    >>> mask_decode("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    (0, 68719476735)
+    >>> mask_decode("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX0")
+    (0, 68719476734)
+    >>> mask_decode("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX1")
+    (1, 68719476735)
+    >>> mask_decode("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX01")
+    (1, 68719476733)
+    >>> mask_decode("1XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    (34359738368, 68719476735)
+    >>> mask_decode("1XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX0")
+    (34359738368, 68719476734)
+    """
+    or_mask = eval('0b' + tristatestring.replace('X','0'))
+    and_mask = eval('0b' + tristatestring.replace('X','1'))
+    return(or_mask, and_mask)
+
+def line_parse(line):
+    """
+    >>> line_parse('mask = 1XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX0')
+    ('mask', 34359738368, 68719476734)
+    >>> line_parse('mem[12] = 28')
+    ('mem', 12, 28)
+    """
+    lvalue, _, rvalue = line.partition(' = ')
+    if lvalue == 'mask':
+        om, am = mask_decode(rvalue)
+        return 'mask', om, am
+    else:
+        addr=int(lvalue[4:-1])
+        return 'mem', addr, int(rvalue)
+
+class DockingComputer(object):
+    """
+    >>> DockingComputer().run_program(EXAMPLE_DOCKING_PROGRAM).sum_memory()
+    165
+    """
+    def __init__(elf):
+        elf._masks = (0, pow(2, 35) - 1)
+        elf._mem={}
+    
+    def sum_memory(elf):
+        return sum(elf._mem.values())
+
+    def run_program(elf, lines):
+        for op, a1, a2 in map(line_parse,lines):
+            if op == 'mask':
+                elf._masks=(a1,a2)
+            elif op == 'mem':
+                elf._mem[a1] = a2 | elf._masks[0] & elf._masks[1]
+        return elf # for method chaining
+
+
