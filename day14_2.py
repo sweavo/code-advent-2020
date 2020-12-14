5debc6f00a58c33550d4e4f5924b80584f3b6c11
Steve Carter <sweavo@gmail.com>
2020-12-14 19:46:12 +0000

    do not know what all the fuss was about ...

--- day14_2.py
+++ day14_2.py
@@ -9,7 +9,12 @@ where each list represents the next bit in the address. When an X is encountered
 we write to both branches, which is what stops it being a tree.
 
 """
-import day14_1
+
+EXAMPLE1=[
+    "mask = 000000000000000000000000000000X1001X",
+    "mem[42] = 100",
+    "mask = 00000000000000000000000000000000X0XX",
+    "mem[26] = 1 " ]
 
 class GlobMemory(object):
     def __init__(elf):
@@ -68,20 +73,54 @@ class GlobMemory(object):
     def values(elf):
         return elf._memory.values()
 
-    @staticmethod
-    def apply_mask(mask, address):
-        """
-        >>> GlobMemory.apply_mask('10X0','0011')
-        '10X1'
-        """
-        def bitdecision(tup):
-            return tup[1] if tup[0]=='0' else tup[0]
-        
-        return ''.join(map(bitdecision, zip(mask, address)))
+    def items(elf):
+        return elf._memory.items()
 
-def run_program(lines):
+def apply_mask(mask, address):
     """
-    >>> run_program(()
-    0
+    >>> apply_mask('10X0','0011')
+    '10X1'
     """
-    mem = 
+    def bitdecision(tup):
+        return tup[1] if tup[0]=='0' else tup[0]
+    
+    return ''.join(map(bitdecision, zip(mask, address)))
+
+def parse_line(line):
+    """
+    >>> parse_line('mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X')
+    'XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X'
+    >>> parse_line('mem[8] = 11')
+    ('000000000000000000000000000000001000', 11)
+    """
+    op, _, arg = line.partition(' = ')
+    if op == 'mask':
+        return arg
+    else:
+        addr=int(op[4:-1])
+        return '{0:036b}'.format(addr), int(arg)
+
+def run_program(memory, lines):
+    """
+    >>> m=GlobMemory()
+    >>> run_program(m, EXAMPLE1)
+    >>> sum(m.values())
+    208
+    """
+    mask='00000000000000000000000000000000000'
+    for operation in map(parse_line, lines):
+        if isinstance(operation,tuple):
+            memory[apply_mask(mask,operation[0])] = operation[1]
+        else:
+            mask = operation
+
+def day14_2():
+    """
+    >>> day14_2()
+    2173858456958
+    """
+    memory = GlobMemory()
+    with open('day14input.txt', 'r') as fp:
+        run_program(memory, fp)
+    return sum(memory.values())
+
