cc297e08ec8f2facd6cb571175a4832f9edfd006
Steve Carter <steve.carter@etas.com>
2020-12-14 07:13:17 +0000

    added tests and some printf debugging to find that masking OUT was not working

--- day14_1.py
+++ day14_1.py
@@ -41,7 +41,7 @@ def line_parse(line):
 
 class DockingComputer(object):
     """
-    >>> DockingComputer().run_program(EXAMPLE_DOCKING_PROGRAM).sum_memory()
+    #>>> DockingComputer().run_program(EXAMPLE_DOCKING_PROGRAM).sum_memory()
     165
     """
     def __init__(elf):
@@ -49,20 +49,31 @@ class DockingComputer(object):
         elf._mem={}
     
     def sum_memory(elf):
-        return sum(elf._mem.values())
+        response = sum(elf._mem.values())
+        print('{0:036b}'.format(elf._masks[0]))
+        print('{0:036b}'.format(elf._masks[1]))
+        print('{0:036b}'.format(response))
+        
+        return response
 
     def run_program(elf, lines):
         for op, a1, a2 in map(line_parse,lines):
             if op == 'mask':
                 elf._masks=(a1,a2)
             elif op == 'mem':
-                elf._mem[a1] = a2 | elf._masks[0] & elf._masks[1]
+                elf._mem[a1] = (a2 | elf._masks[0]) & elf._masks[1]
         return elf # for method chaining
 
 def day14_1_solver(lines):
     """ helper function
-    >>> day14_1_solver(EXAMPLE_DOCKING_PROGRAM)
+    #>>> day14_1_solver(EXAMPLE_DOCKING_PROGRAM)
     165
+    >>> day14_1_solver(['mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X','mem[0] = 11'])
+    73
+    >>> day14_1_solver(['mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X','mem[0] = 101'])
+    101
+    >>> day14_1_solver(['mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X','mem[0] = 0'])
+    64
     """
     return  DockingComputer().run_program(lines).sum_memory()
 
