6a37baec861b50c274a28eaf76ffff99ef3919de
Steve Carter <steve.carter@etas.com>
2020-12-14 14:31:21 +0000

    a fairly ignorant implementation, though with reasonably efficient writing

--- day14_2.py
+++ day14_2.py
@@ -27,8 +27,26 @@ class GlobMemory(object):
         >>> g['10'] = 40
         >>> g['10']
         40
+        >>> g['11X'] = 2 # test the globbing
+        >>> g['1X0'] = 6
+        >>> g['1X0']
+        0
+        >>> g['100']
+        6
+        >>> g['110']
+        6
+        >>> g['101']
+        0
+        >>> g['111'] 
+        2
         """
-        elf._memory[key]=value
+        if 'X' in key:
+            cut = key.index('X')
+            elf[key[:cut] + '0' + key[cut+1:]]=value
+            elf[key[:cut] + '1' + key[cut+1:]]=value
+
+        else:
+            elf._memory[key]=value
 
     def __getitem__(elf,key):
         """ read through subscripti
